8f6665d882dbeff83a1503c980701b4b2d5446e2
Antti Sivula <antti.j.sivula@gmail.com>
2024-10-16 20:04:42 +0300

    add project structure proposal to modvault.go

--- modvault.go
+++ modvault.go
@@ -6,11 +6,24 @@ import (
 	"modvault/config"
 )
 
-// configuration
-// tokens
-//  - handles token caching
-// vault
-// bitwarden
+/* Proposed project structure:
+
+|- modvault
+    |- backend
+        |- backend.go
+        |- bitwarden.go
+        |- vault.go
+    |- config
+        |- config.go
+    |- flags (using Cobra)
+    |- token (if necessary)
+    |- modvault.go
+    |- go.mod
+    |- go.sum
+    |- .gitignore
+    |- README.md
+
+*/
 
 // main gets flags, determines what backend is used, passes flags to backend module via function call
 func main() {

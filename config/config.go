4303dbde0fbd93abb52427d6df591c25f9bce6b8
Antti Sivula <antti.j.sivula@gmail.com>
2024-10-20 16:02:37 +0300

    add function for merging ConfigChoices

--- config/config.go
+++ config/config.go
@@ -5,18 +5,47 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
+	"reflect"
 
 	"modvault/backend"
 )
 
 const defaultConfigFilename = ".modvault.config"
 
-type configJson struct {
-	BackendName string `json:"backend"`
+type ConfigChoices struct {
+	DefaultBackend string `json:"backend"`
+	DefaultPrefix  string `json:"prefix,omitempty"`
+	DefaultTTL     string `json:"ttl"`
+}
+
+func (cc ConfigChoices) MergeOnto(target ConfigChoices) ConfigChoices {
+	var mergedChoices ConfigChoices
+	ccType := reflect.TypeOf(mergedChoices)
+	ccVal := reflect.ValueOf(cc)
+	tVal := reflect.ValueOf(target)
+	resultVal := reflect.ValueOf(&mergedChoices)
+
+	for i := 0; i < ccType.NumField(); i++ {
+		var resultFieldVal string
+		ccFieldVal := ccVal.Field(i)
+		targetFieldVal := tVal.Field(i)
+
+		if ccFieldVal.String() == "" {
+			resultFieldVal = targetFieldVal.String()
+		} else {
+			resultFieldVal = ccFieldVal.String()
+		}
+
+		resultVal.Elem().Field(i).SetString(resultFieldVal)
+	}
+
+	return mergedChoices
 }
 
 type Config struct {
 	Backend backend.Backend
+	Prefix  string
+	TTL     string
 }
 
 func getConfigFilePath() string {
@@ -27,10 +56,10 @@ func getConfigFilePath() string {
 
 // readConfigFile takes a file path as an argument or defaults to the
 // default configuration file. The file should be in JSON format.
-// readConfigFile unmarshals the JSON file into a configJson struct.
+// readConfigFile unmarshals the JSON file into a ConfigChoices struct.
 // If a path is not provided and the default configuration file does
 // not exist, readConfigFile calls promptGenerateConfig.
-func readConfigFile() configJson {
+func readConfigFile() ConfigChoices {
 	// TODO: take file path as argument
 	f, err := os.ReadFile(getConfigFilePath())
 	// TODO: if config file does not exist, prompt config creation
@@ -38,14 +67,15 @@ func readConfigFile() configJson {
 		panic(err)
 	}
 
-	var configFileJson configJson
+	var configFileChoices ConfigChoices
 
-	err = json.Unmarshal(f, &configFileJson)
+	err = json.Unmarshal(f, &configFileChoices)
 	if err != nil {
+		// TODO: proper error message and exit
 		panic(err)
 	}
 
-	return configFileJson
+	return configFileChoices
 }
 
 // GetConfig takes in config values passed in as command line arguments
@@ -53,8 +83,8 @@ func readConfigFile() configJson {
 func GetConfig() Config {
 	// GetConfig
 	// trash implementation
-	configFileJson := readConfigFile()
-	backendChoice, err := backend.GetBackend(configFileJson.BackendName)
+	configFileChoices := readConfigFile()
+	backendChoice, err := backend.GetBackend(configFileChoices.DefaultBackend)
 	if err != nil {
 		panic(err)
 	}
